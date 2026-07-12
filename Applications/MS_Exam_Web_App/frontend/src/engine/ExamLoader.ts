7209455974e06b7116a6a3cd91350e7bb65fd0a4
germain-yao <germainyao.linux@gmail.com>
2026-07-12 23:47:57 +0000

    feat(wordtojson): Connexion front_Backv1

--- Applications/MS_Exam_Web_App/frontend/src/engine/ExamLoader.ts
+++ Applications/MS_Exam_Web_App/frontend/src/engine/ExamLoader.ts
@@ -1,51 +1,26 @@
 import type { Question } from "../types/question";
 
-import sc100 from "../data/questions/sc-100.json";
-import sc200 from "../data/questions/sc-200.json";
 import sc300 from "../data/questions/sc-300.json";
-import sc400 from "../data/questions/sc-400.json";
-
-import az900 from "../data/questions/az-900.json";
-import az104 from "../data/questions/az-104.json";
-import az305 from "../data/questions/az-305.json";
-import az500 from "../data/questions/az-500.json";
-import az700 from "../data/questions/az-700.json";
-import az800 from "../data/questions/az-800.json";
-import az801 from "../data/questions/az-801.json";
 
 export class ExamLoader {
 
-    private exams: Record<string, Question[]> = {
-
-        "sc-100": sc100 as Question[],
-        "sc-200": sc200 as Question[],
-        "sc-300": sc300 as Question[],
-        "sc-400": sc400 as Question[],
-
-        "az-900": az900 as Question[],
-        "az-104": az104 as Question[],
-        "az-305": az305 as Question[],
-        "az-500": az500 as Question[],
-        "az-700": az700 as Question[],
-        "az-800": az800 as Question[],
-        "az-801": az801 as Question[]
-
-    };
-
     public load(certification: string): Question[] {
 
-        const exam =
-            this.exams[certification.toLowerCase()];
+        switch (certification.toLowerCase()) {
 
-        if (!exam) {
+            case "sc-300":
 
-            throw new Error(
-                `Certification inconnue : ${certification}`
-            );
+                return sc300 as Question[];
+
+            default:
+
+                console.warn(
+                    `La certification '${certification}' ne possède pas encore de banque de questions.`
+                );
+
+                return [];
 
         }
-
-        return exam;
 
     }
 
